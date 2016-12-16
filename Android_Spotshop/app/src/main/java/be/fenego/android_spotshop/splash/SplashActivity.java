4f3af3e61b1336025a07b99b94e2c6cd3833d8e1
Nick <nick_philippe@hotmail.com>
2016-12-16 23:26:42 +0100

    [GUI] homepage layout + list_item layout

--- Android_Spotshop/app/src/main/java/be/fenego/android_spotshop/splash/SplashActivity.java
+++ Android_Spotshop/app/src/main/java/be/fenego/android_spotshop/splash/SplashActivity.java
@@ -1,9 +1,11 @@
 package be.fenego.android_spotshop.splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import be.fenego.android_spotshop.R;
+import be.fenego.android_spotshop.home.HomeActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(this, HomeActivity.class));
     }
 }
