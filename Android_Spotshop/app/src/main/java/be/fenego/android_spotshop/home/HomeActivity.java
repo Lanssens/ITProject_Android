f721b1aa6ab54812034f09edc1ae4e0962c6d679
Nick <nick_philippe@hotmail.com>
2016-12-16 14:44:25 +0100

    Projectstructuur [4]

---
+++ Android_Spotshop/app/src/main/java/be/fenego/android_spotshop/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package be.fenego.android_spotshop.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.fenego.android_spotshop.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
