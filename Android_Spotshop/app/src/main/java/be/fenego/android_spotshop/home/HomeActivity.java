2b7c8e14863c5b70c3b031058ce3cd6d4d9daded
Nick <nick_philippe@hotmail.com>
2016-12-21 16:28:18 +0100

    [ProductService] aanmaken productService

--- Android_Spotshop/app/src/main/java/be/fenego/android_spotshop/home/HomeActivity.java
+++ Android_Spotshop/app/src/main/java/be/fenego/android_spotshop/home/HomeActivity.java
@@ -2,14 +2,61 @@ package be.fenego.android_spotshop.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.Console;
+import java.util.ArrayList;
 
 import be.fenego.android_spotshop.R;
+import be.fenego.android_spotshop.model.Product;
+import be.fenego.android_spotshop.service.ProductService;
+import butterknife.BindView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ProductService productService;
+    ProductAdapter productAdapter;
+
+    @BindView(R.id.productListView)
+    ListView productListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        productService = ProductService.retrofit.create(ProductService.class);
+
+
+
+
+
+    }
+
+    private void setProductAdapter(){
+        ArrayList<Product> products = new ArrayList<Product>();
+
+        productService.getProducts().enqueue(new Callback<ArrayList<Product>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Product>> call, Response<ArrayList<Product>> response) {
+                // productAdapter = new ProductAdapter(this, response.body().toArray()); ??
+               // productListView.setAdapter(productAdapter);
+
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Product>> call, Throwable t) {
+
+            }
+        });
+
     }
 }
---
+++ Android_Spotshop/app/src/main/java/be/fenego/android_spotshop/service/ProductService.java
@@ -0,0 +1,35 @@
+package be.fenego.android_spotshop.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
+import be.fenego.android_spotshop.model.Product;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.*;
+
+/**
+ * Created by Nick on 21/12/2016.
+ */
+
+public interface ProductService {
+
+    public static final String BASE_URL = "https://axesso.fenego.zone/INTERSHOP/rest/WFS/inSPIRED-inTRONICS-Site/-";
+
+    @GET("/products/{SKU}")
+    Call<Product> getProduct(@Path("SKU") String SKU);
+
+    @GET("/products")
+    Call<ArrayList<Product>> getProducts();
+
+
+    Gson gson = new GsonBuilder().create();
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build();
+}
